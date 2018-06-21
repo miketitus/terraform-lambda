64d07d223307500e7a9bd40b8bd53a89b1be09ee
miketitus <mike@mike-titus.com>
2018-06-21 16:14:22 -0700

    first commit

---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "aws_access_key" {
+  description = "AWS access key, must pass on command line using -var"
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret access key, must pass on command line using -var"
+}
+
+variable "aws_region" {
+  description = "US West Oregon"
+  default     = "us-west-2"
+}
